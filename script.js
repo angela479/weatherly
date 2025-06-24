da07ef28edc75e57be117f2d7acc9200e84ce9a5
angela479 <angelaa.bera@gmail.com>
2025-06-24 13:21:39 +0530

    updated ui and font

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiKey="7b3bfb316c572b90493c50f51eb086c4";
             const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
             const searchBox=document.querySelector(".search input");
-            const searchBtn=document.querySelector(".search button")
+            const searchBtn=document.querySelector(".search button");
 
 
             async function checkWeather(city) {
@@ -16,6 +16,23 @@ const apiKey="7b3bfb316c572b90493c50f51eb086c4";
             document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
             document.querySelector(".wind").innerHTML=data.wind.speed + " km/h";
             document.querySelector(".clouds").innerHTML=data.clouds.all + "%";
+
+            const sunrise=data.sys.sunrise;
+            const sunset=data.sys.sunset;
+            const currentTime=data.dt;
+            const isDayTime= currentTime>= sunrise && currentTime<sunset;
+
+            updateBackground(isDayTime);
+            }
+            
+            function updateBackground(isDayTime)
+            {
+                const body= document.body;
+                if(isDayTime)
+                    body.style.backgroundImage="url('sunny.jpg')";
+                else
+                    body.style.backgroundImage="url('night.jpg')";
+
             }
 
             searchBtn.addEventListener("click",()=>{
